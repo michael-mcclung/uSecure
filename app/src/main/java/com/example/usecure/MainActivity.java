b2ea1b358e30f9c9abe21ec1a2f8506ee575eb6b
michael-mcclung <michael.mcclung08@gmail.com>
2020-02-27 00:37:24 -0600

    added logo to app image and login page, made forgot pass and sign up page

--- app/src/main/java/com/example/usecure/MainActivity.java
+++ app/src/main/java/com/example/usecure/MainActivity.java
@@ -18,48 +18,79 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    // variables
     private FirebaseAuth mAuth;
+
+    // texts and buttons
+    EditText emailText, passwordText;
+    Button signInbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
+
+        // set the view now
         setContentView( R.layout.activity_main );
-
-        // texts
-        final EditText emailText = findViewById( R.id.emailText );
-        final EditText passwordText = findViewById( R.id.passwordText );
-
-        // buttons
-        Button signInbtn = (Button) findViewById( R.id.signinBtn );
-        Button createBtn = (Button) findViewById( R.id.createBtn );
-        Button forgotBtn = (Button) findViewById( R.id.forgotpasswordBtn );
 
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
 
+        initializeUI();
+
         signInbtn.setOnClickListener( new View.OnClickListener() {
-            String email = emailText.getText().toString();
-            String password = passwordText.getText().toString();
+            @Override
+            public void onClick(View v) {
+                loginUserAccount();
+            }
+        });
+    }
+
+    private void initializeUI() {
+        emailText = findViewById(R.id.emailText);
+        passwordText = findViewById(R.id.passwordText);
+
+        signInbtn = findViewById(R.id.signinBtn);
+    }
+
+    private void loginUserAccount(){
+        // texts
+        emailText = findViewById( R.id.emailText );
+        passwordText = findViewById( R.id.passwordText );
+
+        signInbtn.setOnClickListener( new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                    mAuth.createUserWithEmailAndPassword( email, password ).addOnCompleteListener( MainActivity.this, new OnCompleteListener<AuthResult>() {
-                                @Override
-                                public void onComplete(@NonNull Task<AuthResult> task) {
 
-                                    if (!task.isSuccessful()) {
-                                        Toast.makeText( MainActivity.this, "ERROR", Toast.LENGTH_LONG ).show();
-                                    } else {
-                                        startActivity( new Intent( MainActivity.this, Home.class ) );
-                                        finish();
-                                    }
-                                }
-
-                            }
-                    );
-
-
+                String email = emailText.getText().toString();
+                final String password = passwordText.getText().toString();
+                if (TextUtils.isEmpty( email )){
+                    Toast.makeText( getApplicationContext(),  "Enter your mail address", Toast.LENGTH_SHORT).show();
+                    return;
                 }
+                if (TextUtils.isEmpty( password )) {
+                    Toast.makeText(getApplicationContext(), "Enter your password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                // authenticate user
+                mAuth.signInWithEmailAndPassword( String.valueOf( emailText ), String.valueOf( passwordText ) ).
+                        addOnCompleteListener( MainActivity.this, new OnCompleteListener<AuthResult>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText( getApplicationContext(), "Login successful!", Toast.LENGTH_LONG ).show();
+
+                            Intent intent = new Intent(MainActivity.this, Home.class);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            Toast.makeText( getApplicationContext(), "Login failed! Please try again later.", Toast.LENGTH_LONG ).show();
+                        }
+                    }
+                });
+            }
 
         });
     }
