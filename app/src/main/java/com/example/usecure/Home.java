90ffcd5460e5f7e8e06c7e5c760d95e13b1a95f1
michael-mcclung <michael.mcclung08@gmail.com>
2020-02-27 15:05:13 -0600

    added layout to security feed page, updated usecure title in home & main, and made voice command page/ added to firebase to record and save audio files

--- app/src/main/java/com/example/usecure/Home.java
+++ app/src/main/java/com/example/usecure/Home.java
@@ -1,17 +1,20 @@
 package com.example.usecure;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Home extends AppCompatActivity {
 
-    Button voiceCommandBtn;
+    Button voiceCommandBtn, logoutBtn, securityCamFeedBtn;
     FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +32,30 @@ public class Home extends AppCompatActivity {
                 startActivity( voiceIntent );
             }
         } );
+
+        securityCamFeedBtn = (Button) findViewById( R.id.securityCamFeedBtn );
+        securityCamFeedBtn.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cameraFeedIntent = new Intent(getApplicationContext(), SecurityCameraFeedActivity.class);
+                startActivity( cameraFeedIntent );
+            }
+        } );
+
+        logoutBtn = (Button) findViewById( R.id.logoutBtn );
+        logoutBtn.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logout();
+            }
+        } );
+    }
+
+    public void logout() {
+        mAuth.getInstance().signOut();
+        System.out.println( "User Logged Out" );
+        Intent logoutIntent = new Intent (getApplicationContext(), MainActivity.class);
+        startActivity( logoutIntent );
     }
 
 }
--- app/src/main/java/com/example/usecure/VoiceCommandActivity.java
+++ app/src/main/java/com/example/usecure/VoiceCommandActivity.java
@@ -1,14 +1,126 @@
 package com.example.usecure;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import org.w3c.dom.Text;
+
+import java.io.File;
+import java.io.IOException;
 
 public class VoiceCommandActivity extends AppCompatActivity {
 
+    private Button recordBtn;
+    private TextView RecordBtnTextView;
+
+    private MediaRecorder mRecorder;
+
+    private String mFileName = null;
+
+    private static final String LOG_TAG = "Record_log";
+
+    private StorageReference mStorage;
+
+    private ProgressDialog mProgress;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate( savedInstanceState );
+    protected void onCreate(Bundle savedInstanceStatus) {
+        super.onCreate( savedInstanceStatus );
         setContentView( R.layout.activity_voice_command );
+
+        mProgress = new ProgressDialog( this );
+        mStorage = FirebaseStorage.getInstance().getReference();
+
+        RecordBtnTextView = (TextView) findViewById( R.id.RecordBtnTextView );
+        recordBtn = (Button) findViewById( R.id.recordBtn );
+
+        mFileName = Environment.getExternalStorageDirectory().getAbsolutePath();
+        mFileName +=  "/recorder_audio.3gp";
+
+        recordBtn.setOnTouchListener( new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                if (event.getAction() == MotionEvent.ACTION_DOWN){
+
+                    startRecording();
+                    RecordBtnTextView.setText( "Recording started..." );
+
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+
+                    stopRecording();
+                    RecordBtnTextView.setText( "Recording stopped!" );
+
+                }
+                return false;
+            }
+        } );
+
     }
+
+    private void startRecording() {
+        mRecorder = new MediaRecorder();
+        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mRecorder.setOutputFile(mFileName);
+        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try {
+            mRecorder.prepare();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
+        }
+
+        mRecorder.start();
+    }
+
+    private void stopRecording() {
+        mRecorder.stop();
+        mRecorder.release();
+        mRecorder = null;
+
+        upLoadAudio();
+    }
+
+    private void upLoadAudio() {
+
+        mProgress.setMessage( "Uploading audio..." );
+        mProgress.show();
+
+        StorageReference filepath = mStorage.child( "Audio" ).child( "new_audio.3gp" );
+
+        Uri uri = Uri.fromFile( new File( mFileName ) );
+
+        filepath.putFile( uri ).addOnSuccessListener( new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                mProgress.dismiss();
+                mProgress.setMessage( "Uploading finished." );
+
+            }
+        } );
+
+    }
+
 }
