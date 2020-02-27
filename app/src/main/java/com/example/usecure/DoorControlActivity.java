91c87a22841405ddf95170e9a43cf8eb592fa2e8
michael-mcclung <michael.mcclung08@gmail.com>
2020-02-27 17:53:51 -0600

    added door control activity and the layout of it

--- app/src/main/java/com/example/usecure/DoorControlActivity.java
+++ app/src/main/java/com/example/usecure/DoorControlActivity.java
@@ -3,6 +3,13 @@ package com.example.usecure;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Spinner;
+import android.widget.ToggleButton;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class DoorControlActivity extends AppCompatActivity {
 
@@ -10,5 +17,6 @@ public class DoorControlActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_door_control );
+
     }
 }
