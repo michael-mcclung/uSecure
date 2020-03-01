2f1fd3619432ee934087be8c24236611182192d3
michael-mcclung <michael.mcclung08@gmail.com>
2020-02-29 20:01:32 -0600

    updated manager user, notification, and upload photo activity layouts

--- app/src/main/java/com/example/usecure/ManageUsersActivity.java
+++ app/src/main/java/com/example/usecure/ManageUsersActivity.java
@@ -2,13 +2,53 @@ package com.example.usecure;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ManageUsersActivity extends AppCompatActivity {
+
+
+    Button uploadUserPhotoBtn, updateUserBtn, backToSettingsBtn, deleteUserBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_manage_users );
+
+        uploadUserPhotoBtn = (Button) findViewById( R.id.uploadUserPhotoBtn );
+        uploadUserPhotoBtn.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent doorControlIntent = new Intent( getApplicationContext(), UploadPhotoActivity.class );
+                startActivity( doorControlIntent );
+            }
+        } );
+
+        updateUserBtn = (Button) findViewById( R.id.updateUserBtn );
+        updateUserBtn.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        } );
+
+        deleteUserBtn = (Button) findViewById( R.id.deleteUserBtn );
+        deleteUserBtn.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        } );
+
+        backToSettingsBtn = (Button) findViewById( R.id.backToSettingsBtn );
+        backToSettingsBtn.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent doorControlIntent = new Intent( getApplicationContext(), SettingsActivity.class );
+                startActivity( doorControlIntent );
+            }
+        } );
     }
 }
--- app/src/main/java/com/example/usecure/NotificationsActivity.java
+++ app/src/main/java/com/example/usecure/NotificationsActivity.java
@@ -1,8 +1,19 @@
 package com.example.usecure;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+import com.google.firebase.messaging.RemoteMessage;
 
 public class NotificationsActivity extends AppCompatActivity {
 
