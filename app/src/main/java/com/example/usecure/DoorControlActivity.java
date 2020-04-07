8b5fc48abd26b1fb4a6e9d9504668a06c957a284
Enrique Cervantes <kikillo140498@hotmail.com>
2020-04-07 15:40:30 -0600

    layouts for settings pages

--- app/src/main/java/com/example/usecure/DoorControlActivity.java
+++ app/src/main/java/com/example/usecure/DoorControlActivity.java
@@ -1,10 +1,12 @@
 package com.example.usecure;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -12,8 +14,14 @@ import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DoorControlActivity extends AppCompatActivity {
 
@@ -72,6 +80,25 @@ public class DoorControlActivity extends AppCompatActivity {
                 //mDatabase.child( userUid ).child( "Door Control" ).getRef();
             }
         } );
+        mDatabase.child("Door Control").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                final List<String> doors = new ArrayList<String>();
+                for(DataSnapshot doorSnapshot: dataSnapshot.getChildren()){
+                    String doorName = doorSnapshot.child("doorName").getValue(String.class);
+                    doors.add(doorName);
+                }
+                ArrayAdapter<String> doorAdapter = new ArrayAdapter<String>(DoorControlActivity.this, android.R.layout.simple_spinner_item, doors);
+                doorAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spinnerDropDown.setAdapter(doorAdapter);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
   /*  protected void addNewSpinnerItem(){
