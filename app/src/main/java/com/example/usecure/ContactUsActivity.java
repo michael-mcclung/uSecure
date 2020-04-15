972ec4f2642a8c99ec07ec07485b480f84d862e4
Enrique Cervantes <kikillo140498@hotmail.com>
2020-04-15 15:41:55 -0600

    Submit tickets

--- app/src/main/java/com/example/usecure/ContactUsActivity.java
+++ app/src/main/java/com/example/usecure/ContactUsActivity.java
@@ -6,17 +6,44 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ContactUsActivity extends AppCompatActivity {
 
     Button settingsBtn;
+    Button submitBtn;
+    ticket support;
+    EditText name, email, reason;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_contact_us );
 
+        name = (EditText) findViewById(R.id.editText4);
+        email = (EditText) findViewById(R.id.editText3);
+        reason = (EditText) findViewById(R.id.editText5);
         settingsBtn = (Button) findViewById( R.id.settingsBtn2 );
+        submitBtn = (Button) findViewById(R.id.submitBtn);
+        mDatabase = FirebaseDatabase.getInstance().getReference( "Main User").child("Support tickets");
+        support = new ticket();
+
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                support.setEmail(email.getText().toString().trim());
+                support.setName(name.getText().toString().trim());
+                support.setReason(reason.getText().toString().trim());
+                mDatabase.push().setValue(support);
+                Toast.makeText(ContactUsActivity.this, "Ticket sent", Toast.LENGTH_LONG).show();
+            }
+        });
+
         settingsBtn.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
---
+++ app/src/main/java/com/example/usecure/ticket.java
@@ -0,0 +1,35 @@
+package com.example.usecure;
+
+public class ticket {
+    private String name;
+    private String reason;
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public ticket(){
+
+    }
+}
