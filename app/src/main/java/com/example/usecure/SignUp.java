8b7d7586545b3498800ded5d979cf9383eba7e1f
michael-mcclung <michael.mcclung08@gmail.com>
2020-04-22 17:17:59 -0500

    new upload of comments

---
+++ app/src/main/java/com/example/usecure/SignUp.java
@@ -0,0 +1,19 @@
+// packages
+package com.example.usecure;
+
+// sign up class
+public class SignUp {
+
+    // variables
+    private String email, password, first, last, add, number;
+    
+    // function used to get sign up credentials
+    public SignUp(String email, String password, String first, String last, String add, String number){
+        this.add = add;
+        this.email = email;
+        this.first = first;
+        this.last = last;
+        this.number = number;
+        this.password = password;
+    }
+}
