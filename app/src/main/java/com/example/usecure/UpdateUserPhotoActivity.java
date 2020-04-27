220ce70a46cdbe58cc3bbec6d2c003b9e62763f4
michael-mcclung <michael.mcclung08@gmail.com>
2020-04-27 17:50:37 -0500

    fixed update photo page, if user specifies name of file, firebase storage will update the photo with the same name and delete old photo

--- app/src/main/java/com/example/usecure/UpdateUserPhotoActivity.java
+++ app/src/main/java/com/example/usecure/UpdateUserPhotoActivity.java
@@ -31,10 +31,10 @@ import java.io.IOException;
 
 public class UpdateUserPhotoActivity extends AppCompatActivity {
 
-    private Button backToSettingsBtn, cameraBtn, btnUpload, btnChoose;
+    private Button backToSettingsBtn, btnUpload, btnChoose;
     private ImageView imgView;
     private EditText userSetImageName;
-    int Image_Request_Code = 7;
+    private final int PICK_IMAGE_REQUEST = 71;
 
     // references
     Uri FilePathUri;
@@ -57,21 +57,11 @@ public class UpdateUserPhotoActivity extends AppCompatActivity {
         storageReference = FirebaseStorage.getInstance().getReference();
         databaseReference = FirebaseDatabase.getInstance().getReference(Database_Path);
         userSetImageName = (EditText) findViewById( R.id.userSetImageName );
-        cameraBtn = (Button) findViewById( R.id.cameraBtn );
         backToSettingsBtn = (Button) findViewById(R.id.backToSettingsBtn);
         btnChoose = (Button) findViewById( R.id.btnChoose );
         btnUpload = (Button) findViewById( R.id.btnUpload );
         imgView = (ImageView) findViewById( R.id.imgView );
         progressDialog = new ProgressDialog(getApplicationContext());
-
-        // go to phones camera
-        cameraBtn.setOnClickListener( new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent cameraIntent = new Intent( MediaStore.ACTION_IMAGE_CAPTURE );
-                startActivityForResult( cameraIntent, 0 );
-            }
-        } );
 
         // go back to settings page
         backToSettingsBtn.setOnClickListener(new View.OnClickListener() {
@@ -86,7 +76,7 @@ public class UpdateUserPhotoActivity extends AppCompatActivity {
         btnChoose.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                chooseImg();
+                chooseImage();
             }
         } );
 
@@ -94,108 +84,71 @@ public class UpdateUserPhotoActivity extends AppCompatActivity {
         btnUpload.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                uploadImg();
+                uploadImage();
             }
         } );
     }
 
-    @Override // check android activity result codes
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult( requestCode, resultCode, data );
+    // choose image from phones gallery of photos
+    private void chooseImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
 
-        // if result code is ok, then proceed
-        if (resultCode == RESULT_OK) {
-            Uri selectedImage = data.getData();
-            imgView.setImageURI( selectedImage );
-        }
-
-        // request code check / variables
-        if (requestCode == Image_Request_Code && resultCode == RESULT_OK && data != null && data.getData() != null) {
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null) {
             FilePathUri = data.getData();
             try {
-
-                /* Getting selected image into Bitmap, setting up bitmap selected image into ImageView.
-                   After selecting image change choose button above text */
-                Bitmap bitmapChoose = MediaStore.Images.Media.getBitmap( getContentResolver(), FilePathUri );
-                imgView.setImageBitmap( bitmapChoose );
-                btnChoose.setText( "Image Selected" );
-
-                // catch exceptions
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), FilePathUri);
+                imgView.setImageBitmap(bitmap);
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
     }
 
-    // Creating Method to get the selected image file Extension from File Path URI.
-    public String GetFileExtension(Uri uri) {
-
-        // variables
-        ContentResolver contentResolver = getContentResolver();
-        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
-
-        // Returning the file Extension.
-        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri)) ;
-
-    }
-
-    // choose image function / check android request codes
-    private void chooseImg(){
-        Intent chooseImage = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        startActivityForResult(chooseImage , 1);
-    }
-
     // upload image functions
-    private void uploadImg(){
-        // Checking whether FilePathUri Is empty or not.
-        if (FilePathUri != null) {
-
-            // Setting progressDialog title and showing progressDialog.
-            progressDialog.setTitle("Image is Uploading...");
+    private void uploadImage() {
+        if(FilePathUri != null)
+        {
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Uploading...");
             progressDialog.show();
 
-            // Creating second StorageReference / adding addOnSuccessListener to second StorageReference.
-            StorageReference storageReference2nd = storageReference.child(Storage_Path + System.currentTimeMillis() + "." + GetFileExtension(FilePathUri));
-            storageReference2nd.putFile(FilePathUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            // get what user wants name of image to be
+            String nameOfFile = userSetImageName.getText().toString().trim();
+
+            // uploads to firebase storage
+            StorageReference ref = storageReference.child("images/" + nameOfFile) ;
+
+            // upload file to storage, let user know uploading failed and/or show user percentage of uploading file
+            ref.putFile(FilePathUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
                 @Override
                 public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-
-                    // Getting image name from EditText and store into string variable.
-                    String TempImageName = userSetImageName.getText().toString();
-
-                    // Hiding the progressDialog after done uploading, showing toast message after done uploading.
                     progressDialog.dismiss();
-                    Toast.makeText(getApplicationContext(), "Image Uploaded Successfully ", Toast.LENGTH_LONG).show();
-
-                    @SuppressWarnings("VisibleForTests")
-                    ImageUploadInfo imageUploadInfo = new ImageUploadInfo(TempImageName, storageReference.getDownloadUrl().toString());
-
-                    // Getting image upload ID, adding image upload id s child element into databaseReference.
-                    String ImageUploadId = databaseReference.push().getKey();
-                    databaseReference.child(ImageUploadId).setValue(imageUploadInfo);
+                    Toast.makeText(getApplicationContext(), "Uploading...", Toast.LENGTH_SHORT).show();
                 }
             })
-                    // If something goes wrong
                     .addOnFailureListener(new OnFailureListener() {
                         @Override
-                        public void onFailure(@NonNull Exception exception) {
-
-                            // Hiding the progressDialog, showing exception error message.
+                        public void onFailure(@NonNull Exception e) {
                             progressDialog.dismiss();
-                            Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_LONG).show();
+                            Toast.makeText(getApplicationContext(), "Failed "+e.getMessage(), Toast.LENGTH_SHORT).show();
                         }
                     })
-
-                    // On progress change upload time, setting progressDialog Title.
                     .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
                         @Override
                         public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
-                            progressDialog.setTitle("Image is Uploading...");
+                            double progress = (100.0*taskSnapshot.getBytesTransferred()/taskSnapshot
+                                    .getTotalByteCount());
+                            progressDialog.setMessage("Uploaded "+(int)progress+"%");
                         }
                     });
-        }
-        else {
-            Toast.makeText(getApplicationContext(), "Please Select Image or Add Image Name", Toast.LENGTH_LONG).show();
         }
     }
 
